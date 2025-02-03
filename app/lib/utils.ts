5c5946d81431573eb15726d46078f08ffd076cd1
Adriano Neves Rodrigues <addnrodrigues@gmail.com>
2025-02-03 20:58:25 -0300

    add image compression utility functions and handle image input events

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,3 +1,4 @@
+import imageCompression from "browser-image-compression";
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -18,5 +19,48 @@ export function sanitizeLink(link?: string) {
     .replace(/\s/g, "")
     .replace(/[!@#$%^&*()_+\-=\[\]{};':"\\|,ˆ.<>\/?]+/, "")
     .toLocaleLowerCase();
+}
+
+export const compressImage = (file: File): Promise<File> => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      maxSizeMB: 0.2, // 200KB
+      maxWidthOrHeight: 900,
+      useWebWorker: true,
+      fileType: "image/png",
+    };
+
+    imageCompression(file, options).then((compressedFile) => {
+      resolve(compressedFile);
+    });
+  });
+};
+
+
+
+export function triggerImageInput(id: string) {
+  document.getElementById(id)?.click();
+}
+
+export function handleImageInput(e: React.ChangeEvent<HTMLInputElement>) {
+  const file = e.target.files?.[0] ?? null;
+  if (file) {
+    const imageURL = URL.createObjectURL(file);
+    return imageURL;
+  }
+  return null;
+}
+
+export async function compressFiles(files: File[]) {
+  const compressPromisses = files.map(async (file) => {
+    try {
+      return await compressImage(file);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  });
+
+  return (await Promise.all(compressPromisses)).filter((file) => file !== null);
 }
 
